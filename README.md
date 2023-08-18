2317165732665d4c8b1a983406edacfa7054dd39
neuromagus <neuromagus@mail.ru>
2023-08-18 14:40:52 +0300

    added theme and screenshot

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# righteous-theme
-righteous emacs theme
+## Righteous
+A righteous dark theme for Emacs, inspired by Acme/Solarized themes.
+
+![Screenshot](./screenshot.jpg)
+
+## Installing
+
+copy `righteous-theme.el` to the `~/.config/emacs/themes` and put the following in your Emacs `init.el`:
+
+```elisp
+(add-to-list 'custom-theme-load-path (expand-file-name "~/.config/emacs/themes")
+(load-theme 'righteous t)
+```
+
+Enjoy ;)
---
+++ righteous-theme.el
@@ -0,0 +1,68 @@
+;; righteous-theme.el -- Colorless righteous theme, inspired by ACME/Solarized/One dark themes
+;; version: 1.0
+
+(deftheme righteous "Righteous theme")
+
+(let* ((righteous/bg          "#00181f")
+       (righteous/fg          "#93a1a1")
+       (righteous/comment     "#4e4e4e")
+       (righteous/string      "#5faf87")
+       (righteous/parentmatch "#ff5fff")
+       (righteous/cursor      "#cd390b")
+       (righteous/select      "#767676"))
+
+  (custom-theme-set-faces
+   `righteous
+   `(default ((t (:foreground, righteous/fg :background, righteous/bg))))
+   
+   ;; highlight
+   `(region  ((t (:foreground "black" :background, righteous/select))))
+
+   ;;parents match
+   `(show-paren-match ((t (:foreground ,righteous/parentmatch))))
+
+   ;; cursor   
+   `(cursor ((t (:background, righteous/cursor))))
+    
+   ;; modeline
+   `(linum ((t :foreground, righteous/fg)))
+   `(mode-line ((t (:background, righteous/bg))))
+   
+   ;; hl-line
+   `(hl-line ((t (:foreground, righteous/select ))))
+
+   ;; remove syntax highlight
+   `(font-lock-builtin-face		    ((t (:foreground ,righteous/fg))))
+   `(font-lock-function-name-face	((t (:foreground ,righteous/fg))))
+   `(font-lock-keyword-face		    ((t (:foreground ,righteous/fg))))
+   `(font-lock-preprocessor-face	((t (:foreground ,righteous/fg))))
+   `(font-lock-type-face		    ((t (:foreground ,righteous/fg))))
+   `(font-lock-constant-face		((t (:foreground ,righteous/fg))))
+   `(font-lock-variable-name-face	((t (:foreground ,righteous/fg))))
+   
+   ;; add for strings and comments
+   `(font-lock-string-face		((t (:foreground ,righteous/string))))
+   `(font-lock-comment-face		((t (:foreground ,righteous/comment))))
+   `(font-lock-comment-delimiter-face	((t (:foreground ,righteous/comment))))
+   `(font-lock-doc-string-face ((t (:foreground ,righteous/comment))))
+   `(font-lock-doc-face ((t (:foreground ,righteous/comment))))
+   `(font-lock-preprocessor-face ((t (:foreground ,righteous/comment))))
+   `(font-lock-regexp-grouping-construct ((t (:foreground ,righteous/comment))))
+   
+   ;; warning/errors
+   `(warning ((t (:foreground "red"))))
+   `(error ((t (:foreground "red" :weight bold))))
+   `(font-lock-warning-face		((t (:foreground "red" :bold t))))
+   `(dired-directory ((t (:foreground ,righteous/comment))))))
+
+(when load-file-name
+  (add-to-list 'custom-theme-load-path
+               (file-name-as-directory (file-name-directory load-file-name))))
+
+(defun righteous-theme()
+  "Apply the righteous color theme."
+  (interactive)
+  (load-theme 'righteous t))
+
+(provide 'righteous-theme)
+(provide-theme 'righteous)
